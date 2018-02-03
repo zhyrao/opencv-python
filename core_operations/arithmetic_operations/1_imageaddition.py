9fdc59beb8efa40e92ebdb27e582a5b33804c055
rain330047@outlook.com <rain330047@outlook.com>
2018-02-04 00:07:30 +0800

    arithmetic operations

---
+++ core_operations/arithmetic_operations/1_imageaddition.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-02-03 23:37:30
+# @Author  : Joe
+# @Version : 
+
+import numpy as np
+import cv2
+
+
+# cv2.add()  	(src1, src2, dst, mask=None) → None
+	# 
+# cv2.addWeighted()		(src1, alpha, src2, beta, gamma, dst) → None
