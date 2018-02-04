eadc1ac32c30559a3fe0515848f7cd0f6225ae89
zhyrao <rain330047@outlook.com>
2018-02-04 23:25:25 +0800

    complete image addition

--- core_operations/arithmetic_operations/1_imageaddition.py
+++ core_operations/arithmetic_operations/1_imageaddition.py
@@ -29,3 +29,14 @@ print(res_np)
 #It will be more visible when you add two images. OpenCV function will provide a better result. So always better stick to OpenCV functions.
 
 # cv2.addWeighted()		(src1, alpha, src2, beta, gamma, dst) → None
+# src1:	第一张图片数组
+# alpha: 第一张图片权重比例
+# src2: 第二张图片
+# beta: 第二张图片权重比例
+# dst: 输出数组（保持和输入图片数组有一样的大小和通道数量）
+# gamma: 对相加后数值的一个缩放量（scalar）
+# dtype： 输出数组可选择的depth；如果两个输入图片数组用同样的depth，
+		  #dtype可以设置为-1，这时候于src1.depth()相同
+
+# 公式
+		# ds(i) = saturate(src1(i) * alpha + src2(i) * beta + gamma)
