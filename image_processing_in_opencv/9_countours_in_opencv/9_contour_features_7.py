d049d27aa626f193a12b531a5cdae43d5860e438
zhyrao <rain330047@outlook.com>
2018-02-18 21:24:58 +0800

    Fitting a Line

---
+++ image_processing_in_opencv/9_countours_in_opencv/9_contour_features_7.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-02-18
+# @Author  : Joe
+
+import numpy as np
+import cv2
+
+
+img = cv2.imread('rect.jpg')
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+ret,thresh = cv2.threshold(img_gray, 127, 255, 0)
+
+img_c, contours, hierachy = cv2.findContours(thresh, 0, 1)
+
+cnt = contours[0]
+
+rows, cols = img.shape[:2]
+[vx, vy, x, y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+lefty= int((-x*vy/vx) + y)
+righty= int(((cols-x) * vy/vx)+y)
+img = cv2.line(img, (cols - 1, righty),(0, lefty), (0,255,0),2)
+
+cv2.imshow('ellipse', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
