bfa63557596b227e35ccc5ff4143f4ae989b0f16
zhyrao <rain330047@outlook.com>
2018-04-08 16:16:38 +0800

    render cube instead 3D axis

--- camera calibration and 3d reconstruction/2_pose_estimation.py
+++ camera calibration and 3d reconstruction/2_pose_estimation.py
@@ -28,19 +28,32 @@ import glob
 # # load previously saved data
 with np.load('B.npz') as X:
 	mtx, dist, _,_ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+	
 
 def draw(img, corners, imgpts):
-	corner = tuple(corners[0].ravel())
-	img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
-	img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
-	img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+	print(imgpts)
+	imgpts = np.int32(imgpts).reshape(-1,2)
+	print(imgpts)
+	# draw ground floor in green
+	img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+	# draw pillars in blue color
+	for i,j in zip(range(4),range(4,8)):
+		img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+	# draw top layer in red color
+	img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
 	return img
+	# corner = tuple(corners[0].ravel())
+	# img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+	# img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+	# img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+	# return img
 
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6*7, 3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-axis = np.float32([[3,0,0],[0,3,0],[0,0,-3]]).reshape(-1,3)
+axis = np.float32([[0,0,0], [0,3,0], [3,3,0], [3,0,0],
+                   [0,0,-3],[0,3,-3],[3,3,-3],[3,0,-3] ]).reshape(-1,3)
 
 for fname in glob.glob('left*.jpg'):
 	img = cv2.imread(fname)
